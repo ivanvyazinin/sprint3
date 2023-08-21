89170449c2c052cb67e34568ef0d3fa687e55993
Ivan Vyazinin <ivan.vyazinin@humans.net>
2023-08-21 19:24:08 +0300

    init

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+}
